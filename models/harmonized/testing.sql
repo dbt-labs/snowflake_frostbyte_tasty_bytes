0a4e995f7f3e93d32b9e7d346d89b5cc9a32ea97
adithya.nanduri@snowflake.com <adithya.nanduri@snowflake.com>
2023-03-17 15:29:12 +0000

    deleted testing files

--- models/harmonized/testing.sql
+++
@@ -1,9 +0,0 @@
-with customer_metrics as (
-    SELECT order_id,
-           item_id,
-           od.ORDER_AMOUNT
-        from {{ ref('stg_order_detail') }} od
-        join {{ ref('stg_order_header') }} oh   
-            on od.order_id = oh.order_id
-)
-Select * from customer_metrics
