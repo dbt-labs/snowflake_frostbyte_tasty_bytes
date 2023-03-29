442f8a40f51ad0f5b1c98256080e5b70d6064ae1
Rabi Abbasi <rabi.abbasi@fishtownanalytics.com>
2023-03-29 11:02:30 +0000

    Added test and train datasets

---
+++ models/ml/model_training/demand_forecast_test.py
@@ -0,0 +1,9 @@
+import snowflake.snowpark.functions as F
+
+def model(dbt, session):
+
+    df = dbt.ref("feature_engineer_prepare_data")
+
+    df = df.where(F.col("split_type") == "test")
+
+    return df
