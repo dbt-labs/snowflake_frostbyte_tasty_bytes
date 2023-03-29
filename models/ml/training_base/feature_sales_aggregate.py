8f6981321f7d027b25ecf2bc968867d9c83f761d
Rabi Abbasi <rabi.abbasi@fishtownanalytics.com>
2023-03-29 07:12:39 +0000

    FIX

--- models/ml/training_base/feature_sales_aggregate.py
+++ models/ml/training_base/feature_sales_aggregate.py
@@ -7,6 +7,43 @@ def model(dbt, session):
         packages = ["pandas"]
     )
 
-    orders_ref = dbt.ref("orders_v")
+    orders_ref = dbt.ref("orders_v").to_pandas()
 
-    return orders_ref
+    ## Create Aggregate Table
+    # Define features
+    order_features = [
+        "date",
+        "shift",
+        "primary_city",
+        "location_id",
+        "latitude",
+        "longitude",
+        "truck_id",
+        "menu_type",
+        "menu_item_id",
+        "unit_price",
+    ]
+
+    # Define target
+    target = ["quantity"]
+
+    # Create DataFrame and define AM/PM shifts
+    df_base = (
+        orders_ref
+        .where(F.col("date") <= max_date)
+        .with_column(
+            "shift",
+            F.iff(F.builtin("DATE_PART")("HOUR", F.col("order_ts")) < "15", "AM", "PM")
+        )
+    )
+
+    # Aggregate
+    df_base = df_base.group_by(order_features).agg(F.sum(*target).alias("quantity"))
+
+    # Calculate sales
+    df_base = df_base.with_column(
+        "sales", F.round(F.col("quantity") * F.col("unit_price"), 2)
+    )
+
+
+    return df_base
