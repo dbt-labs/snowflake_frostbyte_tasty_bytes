3df66f204579754fa7f66e84343ca961b9d6fa89
marie.coolsaet@snowflake.com <marie.coolsaet@snowflake.com>
2023-04-05 15:07:20 +0000

    Changed train test split to be a 30 day testing holdout instead of random split.

--- models/ml/model_training/feature_engineer_prepare_data.py
+++ models/ml/model_training/feature_engineer_prepare_data.py
@@ -31,9 +31,10 @@ def model(dbt, session):
     df = df.with_column("shift", F.iff(F.col("shift") == "AM", 1, 0))
 
     # Split into training and testing
-    train_df, test_df = df.randomSplit([0.8, 0.2])
+    max_date = df.select(F.max("date")).collect()[0][0]
+    df = df.with_column("split_type",
+                        F.iff(F.col("date") <= (max_date - datetime.timedelta(days=30)),
+                                F.lit("train"),
+                                F.lit("test"))).drop("date")
 
-    train_df = train_df.with_column('split_type', lit('train'))
-    test_df = test_df.with_column('split_type', lit('test'))
-
-    return train_df.union(test_df)
+    return df
