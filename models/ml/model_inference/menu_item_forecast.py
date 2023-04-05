7616e6ffa39b071f1a24512dd8bc8e38720140d4
Rabi Abbasi <rabi.abbasi@fishtownanalytics.com>
2023-04-05 03:59:13 +0000

    fix

--- models/ml/model_inference/menu_item_forecast.py
+++ models/ml/model_inference/menu_item_forecast.py
@@ -6,7 +6,7 @@ def model(dbt, session):
     df_future_dates = dbt.ref("get_sales_short_term_trends")
 
     #this is to reference the UDF creation 
-    demand_forecast_udf = dbt.ref("model_training")
+    demand_forecast_udf = dbt.ref("demand_forecast_train_metrics")
 
     feature_cols = df_future_dates.drop(
         "MENU_ITEM_ID",
